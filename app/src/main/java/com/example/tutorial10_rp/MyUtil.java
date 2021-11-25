2863ab29e01a2ebf7f1a29a9f169c1519f928eb9
LENOVO <rpithava575@rku.ac.in>
2021-11-25 10:40:11 +0530

    Tutorial 10

---
+++ app/src/main/java/com/example/tutorial10_rp/MyUtil.java
@@ -0,0 +1,8 @@
+package com.example.tutorial10_rp;
+
+import org.json.JSONArray;
+
+public class MyUtil {
+    public static JSONArray jsonArray = null;
+    public static final String URL_USERS = "https://jsonplaceholder.typicode.com/users";
+}
